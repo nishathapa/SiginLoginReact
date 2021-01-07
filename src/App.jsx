f9143eea5d4349dc42f1b26170b2ac37388b4469
Nisha Thapa <nishathapa4727@gmail.com>
2021-01-07 22:24:37 +0530

    Sigin Login Page using React

---
+++ src/App.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import './App.scss';
+import { Login, Register } from "./components/login/index";
+
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      isLogginActive: true
+    }
+  
+  }
+
+  changeState() {
+    const { isLogginActive } = this.state;
+    if(isLogginActive){
+      this.RightSide.classList.remove("right");
+      this.RightSide.classList.add("left");
+    } else {
+      this.RightSide.classList.remove("left");
+      this.RightSide.classList.add("right");
+    }
+
+    this.setState(prevState => ({ isLogginActive: !prevState.isLogginActive})) 
+  }
+
+
+  render(){
+    const { isLogginActive } = this.state;
+    const current = isLogginActive ? "Register" : "Login";
+    const currentActive = isLogginActive ? "Login" : "Register";
+    return(
+      <div className="App">
+        <div className="login">
+          <div className="container">
+            { isLogginActive && <Login containerRef= {(ref) => this.current = ref}/> }
+            { !isLogginActive && <Register containerRef= {(ref) => this.current = ref}/>}
+            
+          </div>
+          <RightSide current={current} containerRef = {ref => this.RightSide = ref} onClick={this.changeState.bind(this)}/>
+        </div>
+      </div>
+    )
+  }
+}
+
+const RightSide = props => {
+  return <div className="right-side" ref={props.containerRef} onClick={props.onClick}>
+    <div className="inner-container">
+      <div className="text">{props.current}</div>
+    </div>
+  </div>
+}
+
+export default App;
---
+++ src/components/login/login.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import loginImg from "../../login.svg";
+
+export class Login extends React.Component {
+
+    constructor(props){
+        super(props);
+    }
+
+    render() {
+        return(
+            <div className="base-container" ref={this.props.containerRef}>
+                <div className="header">Login</div>
+                <div className="content">
+                    <div className="image">
+                        <img src={loginImg} />
+
+                    </div>
+
+                    <div className="form">
+                        <div className="form-group">
+                            <label htmlFor="username">Username</label>
+                            <input type="text" name="username" placeholder="username"></input>
+                        </div>
+
+                        <div className="form-group">
+                            <label htmlFor="password">Password</label>
+                            <input type="text" name="password" placeholder="Password"></input>
+                        </div>
+                    </div>
+                </div>
+                <div className="footer">
+                    <button type="button" className="btn">Login</button>
+                </div>
+            </div>
+        )
+    }
+}
